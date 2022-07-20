6c14ee3675799a4216235e95af49d0da4d01f037
Kirill Zabrodin <me@kirillzabrodin.com>
2022-07-20 15:35:14 +0100

    added ransom note

---
+++ ransom_note.py
@@ -0,0 +1,42 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        keys = {"a": 0,
+                "b": 0,
+                "c": 0,
+                "d": 0,
+                "e": 0,
+                "f": 0,
+                "g": 0,
+                "h": 0,
+                "i": 0,
+                "j": 0,
+                "k": 0,
+                "l": 0,
+                "m": 0,
+                "n": 0,
+                "o": 0,
+                "p": 0,
+                "q": 0,
+                "r": 0,
+                "s": 0,
+                "t": 0,
+                "u": 0,
+                "v": 0,
+                "w": 0,
+                "x": 0,
+                "y": 0,
+                "z": 0,
+               }
+        for c in magazine:
+            keys[c] = keys[c] + 1
+            
+        for c in ransomNote:
+            keys[c] = keys[c] - 1
+            if keys[c] < 0:
+                return False
+            
+        return True
+        
+        
+res = Solution()
+print(res.canConstruct("money", "myownnieceescapedyesterday"))
