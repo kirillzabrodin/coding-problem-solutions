47389e505e772ffd4a38719750bf79d70af16e72
Kirill Zabrodin <me@kirillzabrodin.com>
2022-07-21 12:05:17 +0100

    fixed pascals triangle

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -1,7 +1,14 @@
-from multiprocessing.dummy import Array
-
-
 def generate(numRows):
-        for i in range(numRows):
-            row = Array[i]
-            total += generateRow(i)
+        if numRows == 1:
+            row = [1]
+            return [row]
+        else:
+            previous_rows = generate(numRows - 1)
+            row = [None] * numRows
+            row[0], row[-1] = 1, 1
+            for i in range(len(row) - 2):
+                row[i + 1] = previous_rows[-1][i] + previous_rows[-1][i + 1]
+        print(previous_rows)
+        return previous_rows + [row]
+    
+print(generate(8))
